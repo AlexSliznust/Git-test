52544c7ddbc364af09f65db9e4d5bb15e21b7589
AlexSliznust <alexanderchernyshev1990@gmail.com>
2025-09-08 11:04:02 +0300

    third commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let text = 'test';
+  let prop = 'green';
+
+  return <>
+  
+  <p className={prop}>{text}</p>
+  
+  </>
+  
+  
+ 
+}
+
+export default App
