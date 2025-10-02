ccec71e77d64ff65cd38e81604c62d2ce66ca568
AlexSliznust <alexanderchernyshev1990@gmail.com>
2025-10-02 11:48:11 +0300

    my first commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,19 +2,24 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useState } from 'react';
+const users = [
+	{name: 'user1', surn: 'surn1', age: 30},
+	{name: 'user2', surn: 'surn2', age: 31},
+	{name: 'user3', surn: 'surn3', age: 32},
+];
+
 
 function App() {
-  let text = 'test';
-  let prop = 'green';
-
-  return <>
-  
-  <p className={prop}>{text}</p>
-  
+	const res = users.map(function(item, index, age) {
+		return <ul>
+			<li>{item.name}</li>:
+			<li>{index.surn}</li>
+      <li>{age.age}</li>
+		</ul>;
+	});
+	return <>
+  {res}
   </>
-  
-  
- 
 }
-
 export default App
